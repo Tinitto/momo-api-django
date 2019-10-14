a83aa8970ea5ce838dec542ebe92f2328bc9bde8
Martin Ahindura <martinahindura@gmail.com>
2019-10-14 04:29:04 +0300

    Add the BaseProduct class

--- momo_sdk/base_momo_product.py
+++ momo_sdk/base_momo_product.py
@@ -1,103 +1,58 @@
 """
-This module holds the base product for all Momo Pay Products 
+This module holds the base product for all Momo Pay Products
 i.e. Collections, Disbursements, Remittances etc.
 It is ported by Martin Ahindura from the mtn-pay-js package
 https://github.com/sopherapps/mtn-pay-js/blob/master/src/core/index.ts
 """
-
-"""
-import getResources, { IResource } from '../utils/repository';
-
-export interface IApiToken {
-  accessToken: string;
-  tokenType: string;
-  expiresIn: number;
-  createdAt: Date;
-}
+import base64
+from datetime import datetime
+from .utils.repository import RemoteResource
 
 
-/**
- * Base class for all products including account,
- * paymentRequests, remittances and disbursements
- * @property {
- * 'Content-Type': string,
- * 'Ocp-Apim-Subscription-Key': string,
- * 'Authorization': string,
- * 'X-Target-Environment': string,} commonHeaders
- * @property {IApiToken | undefined} apiToken
- * @method authenticate()
- * @method shouldAuthenticate()
- * @method secondsSince()
- */
-export class BaseProduct {
-  public commonHeaders: {
-    'Content-Type': string;
-    'Ocp-Apim-Subscription-Key': string;
-    Authorization: string;
-    'X-Target-Environment': string;
-  };
-  protected apiToken: IApiToken | undefined;
-  private authResource: IResource;
+def seconds_since(starting_time):
+    """
+    Returns the number of seconds between now and the given startingTime
+    """
+    return (datetime.now() - starting_time).seconds
 
-  constructor({
-    authBaseURL,
-    authUrl = 'token/',
-    subscriptionKey,
-    apiuserId,
-    apiKey,
-    targetEnvironment = 'sandbox',
-  }: {
-    authBaseURL: string;
-    authUrl?: string;
-    subscriptionKey: string;
-    apiuserId: string;
-    apiKey: string;
-    targetEnvironment?: string;
-  }) {
-    authBaseURL = authBaseURL || `https://ericssonbasicapi2.azure-api.net/collection`;
-    this.commonHeaders = {
-      Authorization: `Basic ${btoa(`${apiuserId}:${apiKey}`)}`,
-      'Content-Type': 'application/json',
-      'Ocp-Apim-Subscription-Key': subscriptionKey,
-      'X-Target-Environment': targetEnvironment,
-    };
-    this.authResource = getResources([authUrl], authBaseURL, this.commonHeaders)[authUrl];
-  }
 
-  /**
-   * Authenticates with remote api if the apiToken is expired or non-existent
-   */
-  protected async authenticate() {
-    if (this.shouldAuthenticate) {
-      const response = await this.authResource.create({});
-      if ([200, 201, 202].includes(response.status)) {
-        this.apiToken = {
-          accessToken: response.data.access_token,
-          createdAt: new Date(),
-          expiresIn: response.data.expires_in,
-          tokenType: response.data.token_type,
-        };
-      }
-    }
-  }
+class BaseProduct:
+    """
+    Base class for all products including account,
+    paymentRequests, remittances and disbursements
+    """
 
-  /**
-   * Checks whether the apiToken has expired or not; or whether it is exists or not
-   */
-  protected shouldAuthenticate() {
-    if (!this.apiToken) {
-      return true;
-    }
-    const tokenAgeInSeconds = this.secondsSince(this.apiToken.createdAt);
-    return tokenAgeInSeconds >= this.apiToken.expiresIn;
-  }
+    def __init__(self,
+                 subscription_key, api_key, apiuser_id,
+                 auth_base_url='https://ericssonbasicapi2.azure-api.net/collection',
+                 auth_url='token/', target_environment='sandbox'):
+        authorization_header = base64.b64encode(
+            "{apiuser_id}: {api_key}".format(apiuser_id=apiuser_id, api_key=api_key))
+        self.common_headers = {
+            'Authorization': "Basic {}".format(authorization_header),
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': subscription_key,
+            'X-Target-Environment': target_environment,
+        }
+        self.auth_resource = RemoteResource(
+            auth_url, auth_base_url, common_headers=self.common_headers)
+        self.api_token = None
 
-  /**
-   * Returns the number of seconds between now and the given startingTime
-   * @param startingTime
-   */
-  protected secondsSince(startingTime: Date) {
-    return (new Date().getTime() - startingTime.getTime()) / 1000;
-  }
-}
-"""
+    def authenticate(self):
+        """
+        Authenticates with remote api if the apiToken is expired or non-existent
+        """
+        if (self.should_authenticate()):
+            response = self.auth_resource.create({})
+            if (response.status_code in [200, 201, 202]):
+                self.api_token = response.json()
+                self.api_token['created_at'] = datetime.now()
+
+    def should_authenticate(self):
+        """
+        Checks whether the apiToken has expired or not; or whether it is exists or not
+        """
+        if (self.api_token is None):
+            return True
+        token_age_in_seconds = seconds_since(self.api_token.created_at)
+        return token_age_in_seconds >= self.api_token.expires_in
